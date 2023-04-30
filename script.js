9d9c186e33927c520b7196ffe31dc151a626a74c
nikleodeon <nikleodeon@users.noreply.github.com>
2023-04-30 08:15:18 -0700

    Update display with number button push

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const keys = document.getElementsByTagName("button");
+const display = document.getElementById("display");
+
+function calcDisplay(e) {
+
+}
 
 for (let i = 0; i < keys.length; i++) {
     keys[i].addEventListener("click", e => {
         const key = e.target;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
         if (key.classList.contains("number")) {
+            if (displayedNum === "0") {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
             console.log("this is a number");
         } else if (key.classList.contains("operation")) {
             console.log("this is an operator");
