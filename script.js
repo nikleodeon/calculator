b817f0ef40a1ae120650509dc86378f24743d00c
nikleodeon <nikleodeon@users.noreply.github.com>
2023-05-03 17:09:23 -0700

    Add decimal functionality

--- script.js
+++ script.js
@@ -1,4 +1,104 @@
-const keys = document.getElementsByTagName("button");
+let clear = document.querySelector(".all-clear");
+let deleteNum = document.querySelector(".delete");
+let equal = document.querySelector(".equal");
+let decimal = document.querySelector(".decimal");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operation");
+let prevDisplay = document.querySelector(".prev-display");
+let currDisplay = document.querySelector(".curr-display");
+let operator = "";
+let prevValue = "";
+let currValue = "";
+
+numbers.forEach((number) => number.addEventListener("click", function(e) {
+    displayNumber(e.target.textContent);
+    currDisplay.textContent = currValue;
+}))
+
+operators.forEach((op) => op.addEventListener("click", function(e) {
+    if (!prevValue.includes("+") && !prevValue.includes("-") && !prevValue.includes("×") && !prevValue.includes("/")) {
+        displayOperator(e.target.textContent);
+        prevDisplay.textContent = prevValue + " " + operator;
+        currDisplay.textContent = currValue;
+    }
+}))
+
+clear.addEventListener("click", function() {
+    prevValue = "";
+    currValue = "";
+    operator = "";
+    prevDisplay.textContent = "";
+    currDisplay.textContent = "0";
+})
+
+equal.addEventListener("click", function() {
+    if (currValue != 0 && prevValue != "") {
+        operate();
+        prevDisplay.textContent = "";
+        if (prevValue.length <= 5) {
+            currDisplay.textContent = prevValue;
+        } else {
+            currDisplay.textContent = prevValue.slice(0,5);
+        }
+    }
+})
+
+decimal.addEventListener("click", function() {
+    addDecimal();
+})
+
+deleteNum.addEventListener("click", function() {
+    removeDigit();
+})
+
+function displayNumber(num) {
+    if (currValue.length < 6) {
+        currValue += num;
+    }
+}
+
+function displayOperator(op) {
+    operator = op;
+    prevValue = currValue;
+    currValue = "";
+}
+
+function operate() {
+    prevValue = Number.parseFloat(prevValue);
+    currValue = Number.parseFloat(currValue);
+
+    if (operator === "+") {
+        prevValue += currValue;
+    } else if (operator === "-") {
+        prevValue -= currValue;
+    } else if (operator === "×") {
+        prevValue *= currValue;
+    } else if (operator === "/") {
+        prevValue /= currValue;
+    }
+
+    prevValue = prevValue.toString();
+    currValue = prevValue.toString();
+}
+
+function addDecimal() {
+    if(!currValue.includes(".")) {
+        currValue += ".";
+    }
+    currDisplay.textContent = currValue;
+}
+
+function removeDigit() {
+    currValue = currValue.substring(0, currValue.length - 1);
+    if (currValue.length === 0) {
+        currValue = "";
+        currDisplay.textContent = "0";
+    } else {
+        currDisplay.textContent = currValue;
+    }
+}
+
+/* const keys = document.getElementsByTagName("button");
 const display = document.getElementById("display");
 
 function calcDisplay(e) {
@@ -16,8 +116,8 @@ for (let i = 0; i < keys.length; i++) {
             } else {
                 display.textContent = displayedNum + keyContent;
             }
-            console.log("this is a number");
         } else if (key.classList.contains("operation")) {
+            display.textContent = displayedNum + " " + keyContent + " ";
             console.log("this is an operator");
         } else if (key.classList.contains("delete")) {
             console.log("this is delete");
@@ -26,7 +126,7 @@ for (let i = 0; i < keys.length; i++) {
         } else if (key.classList.contains("equal")) {
             console.log("this is equal");
         } else if (key.classList.contains("decimal")) {
-            console.log("this is decimal");
+            display.textContent = displayedNum + ".";
         }
     })
-}
+} */
